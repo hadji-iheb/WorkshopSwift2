c2f0ac3ac2200c7056ae849798666e1ff4d89f32
Gdahboy <iphonegdah@yahoo.fr>
2018-10-26 22:00:02 +0100

    add the file + add the function and the tool we need to show for tomorrow

---
+++ WorkshopSWift/Map.swift
@@ -0,0 +1,9 @@
+//
+//  Map.swift
+//  WorkshopSWift
+//
+//  Created by abderrahim gdah on 10/26/18.
+//  Copyright © 2018 abderrahim gdah. All rights reserved.
+//
+
+import Foundation
---
+++ WorkshopSWift/functionfile.swift
@@ -0,0 +1,42 @@
+//
+//  functionfile.swift
+//  WorkshopSWift
+//
+//  Created by abderrahim gdah on 10/26/18.
+//  Copyright © 2018 abderrahim gdah. All rights reserved.
+//
+
+import Foundation
+
+// func 3edia
+func abderrahim (person : String ) -> String {
+    return person
+}
+// func traja3 bcp de valeur lazmena nekhdemou 3leha barcha il fazet car importante barcha
+func hello (person1 : String , person2 : String) -> (Greeting1 : String , Greeting2 : String ) {
+    return ("hello \(person1)" , "hello \(person2)")
+}
+// print func
+// print(hello(person1: "aaa", person2:   "dasfdv"))
+//variadic func ... bon ena hathi mna3refchi min 9bel il idee mte3ha kifeh ndkhalou ensemble de donne fi paramtere yasser 7lowa
+func arithmeticMean(_ numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total / Double(numbers.count)
+}
+// func for mutating paramters
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+// func is like a type
+func somme(_ a : Int , _ b : Int ) -> Int  {
+    return a + b
+}
+var mathfunction : (Int , Int ) -> Int = somme
+
+
+
