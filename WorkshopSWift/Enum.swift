d478da7cecb0d54a1153e2a0b1f563e9cb2aa567
hadji-iheb <ihebhadji19@gmail.comm>
2018-10-29 13:46:00 +0100

    Enumeration

---
+++ WorkshopSWift/Enum.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+
+enum CompassPoint {
+    case north
+    case south
+    case east
+    case west
+}
+
+enum Planet {
+
+    case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+//swift 4.2
+//xcode 10
+//c'est pour utiliser .allCases
+
+//enum Planet1: CaseIterable {
+//
+//    case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
+//}
+
+enum Planet2: Int {
+    
+    case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+
+
