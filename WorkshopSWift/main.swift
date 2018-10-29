e45ad97f9bf55c22810c9616b61415be5f91921b
hadji-iheb <ihebhadji19@gmail.comm>
2018-10-29 13:44:12 +0100

    Enumeration

--- WorkshopSWift/main.swift
+++ WorkshopSWift/main.swift
@@ -14,6 +14,34 @@ print(surfer1.FirstName)
 print(surfer2.FirstName)
 
 
+//
 
 
+var directionToHead = CompassPoint.west
+switch directionToHead {
+case .north:
+    print("Lots of planets have a north")
+case .south:
+    print("Watch out for penguins")
+case .east:
+    print("Where the sun rises")
+case .west:
+    print("Where the skies are blue")
+}
 
+
+let somePlanet = Planet.earth
+switch somePlanet {
+case .earth:
+    print("Mostly harmless")
+default:
+    print("Not a safe place for humans")
+}
+
+
+//for Pl in Planet.allCases {
+//    print(Pl)
+//}
+
+let earthsOrder = Planet2.mars.rawValue
+print ("earth order : \(earthsOrder)")
